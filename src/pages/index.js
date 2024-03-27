9fa6f8a61414e681bb0bf018a857c8c497401dc2
suzanbulbul <suzan.bulbul@anymile.io>
2024-03-28 00:47:45 +0300

    added header

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Home  from './Home/home'
+import Header  from '../components/header'
+
+export default function index() {
+  return (
+   <div>
+    <Header />
+    <Home />
+   </div>
+  )
+}
