078a2de8b9a84f3e3c549015589d56c1aca9d06c
suzanbulbul <suzan.bulbul@anymile.io>
2024-04-02 09:24:37 +0300

    dev: added env file

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -8,12 +8,12 @@ import { v4 } from 'uuid';
 import  toaster  from 'react-hot-toast';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBmSWMO81EqNkCIqce5uWvdd-LNWtbOi8",
-  authDomain: "sb-shop-21cf4.firebaseapp.com",
-  projectId: "sb-shop-21cf4",
-  storageBucket: "sb-shop-21cf4.appspot.com",
-  messagingSenderId: "5400733834",
-  appId: "1:5400733834:web:cf9e23aa8a76d8f2372573"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
