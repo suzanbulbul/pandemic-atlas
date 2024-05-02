2640c3f8499b02b0f9e7b3d79b7410496543ebce
suzanbulbul <suzan.bulbul@anymile.io>
2024-05-02 14:38:36 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Map from '../components/Map'
 
 const Index = () => {
   return (
-   <div>
+   <div> 
     <h1 className="text-blue-500">Pandemic Atlas</h1>
     <Map lng={32.836956} lat={39.925018} zoom={14} />
 
