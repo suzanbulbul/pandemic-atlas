0298d3e6c0097dbe417108d51bf9075992e448f3
suzanbulbul <suzan.bulbul@anymile.io>
2024-05-06 11:02:34 +0300

    fix: revised UI for detail page

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+const Divider = () => (
+  <svg
+    className="inline-block w-full"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 358 2"
+    fill="none"
+  >
+    <path d="M0 1H357.333" stroke="#E2E4E9" />
+  </svg>
+);
+
+export default Divider;
